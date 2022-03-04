cac36d4d577da6450a3ffa39f0e1b46cef7cbffb
Aryan Vora <aryanv20@iitk.ac.in>
2022-03-04 15:30:06 +0530

    Login View

--- Backend/Login/views.py
+++ Backend/Login/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import database
 # Create your views here.
+
+def login_view(request,*args,**kwargs):
+    return render(request,"login.html",{})
--- Backend/src/urls.py
+++ Backend/src/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Login.views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('login/',login_view)
+    path('login/',login_view)
 ]
