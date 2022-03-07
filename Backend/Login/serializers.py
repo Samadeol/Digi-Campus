66797ecd6322ba9efcb075f0ee5020c28d696d7c
Samarth Arora <samartharora03@gmail.com>
2022-03-07 15:27:51 +0530

    changes

--- Backend/Login/serializers.py
+++ Backend/Login/serializers.py
@@ -1,8 +1,8 @@
-from dataclasses import fields
-from rest_framework import serializers
-from .models import Profile
+# from dataclasses import fields
+# from rest_framework import serializers
+# from .models import Profile
 
-class ProfileSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ('full_name')
+# class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Profile
+#         fields = ('full_name')
--- Backend/Login/views.py
+++ Backend/Login/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render, redirect
-from .serializers import ProfileSerializer
+#from .serializers import ProfileSerializer
 from .models import Profile
-from rest_framework import viewsets
+#from rest_framework import viewsets
 # Create your views here.
-class ProfileViewSet(viewsets.ModelViewSet):
-    queryset = Profile.objects.all().order_by('full_name')
-    serializer_class = ProfileSerializer
 
 def login_view(request,*args,**kwargs):
+    profiles = Profile.objects.all()
+    for pro in profiles:
+        print (pro.login)
+        if pro.login == True:
+            return redirect("../mess/")
     if request.method == "POST" :
         user = request.POST.get('User ID')
         passw = request.POST.get('password')
         #pro = profile.ob
-        profiles = Profile.objects.all()
+        #profiles = Profile.objects.all()
         for pro in profiles:
-            if pro.username == user and pro.password ==passw :
+            if pro.username == user and pro.password ==passw:
+                pro.login = True
                 return redirect("../mess/")
         #print(username+ " " + password)
     return render(request,"login.html",{})
