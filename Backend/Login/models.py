6f17f1551ad09957ca44ef067852899f63338d3f
Samarth Arora <samartharora03@gmail.com>
2022-03-07 14:52:19 +0530

    small_fixes

--- Backend/Login/models.py
+++ Backend/Login/models.py
@@ -1,13 +1,6 @@
 from click import password_option
 from django.db import models
 
-# occupation_choice = (
-#     ('Student','Student'),
-#     ('Staff','Staff'),
-#     ('Prof','Proffesor')
-
-# )
-# Create your models here.
 class Profile(models.Model) :
     username = models.CharField(max_length=15)
     full_name = models.CharField(max_length=100)
@@ -16,7 +9,6 @@ class Profile(models.Model) :
     is_student = models.BooleanField()
     is_staff = models.BooleanField()
     is_prof = models.BooleanField()
-    #Occupation = models.Choices(occupation_choice)
     def __str__(self):
         return self.username
     
