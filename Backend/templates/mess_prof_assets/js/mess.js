0c12e0af3b790dc8e8dffb940e4455717fadff83
SARTHAK811 <82260982+SARTHAK811@users.noreply.github.com>
2022-03-14 19:01:17 +0530

    ADD-Extras section done

--- Backend/templates/mess_prof_assets/js/mess.js
+++ Backend/templates/mess_prof_assets/js/mess.js
@@ -1,4 +1,6 @@
 function increment_value_1(){
+    document.getElementById('number_quant_1').style.color='black';
+    document.getElementById('temp_quant_1').style.color='black';
     
     var count=parseInt(document.getElementById('number1').value,10);
     
@@ -13,6 +15,11 @@ function decrement_value_1(){
     if(count>0) count=count-1;
     document.getElementById('number1').value=count;
     document.getElementById('number_quant_1').innerHTML=count;
+    if(count==0){
+        document.getElementById('number_quant_1').style.color='rgba(33,37,41,0.35)';
+        document.getElementById('temp_quant_1').style.color='rgba(33,37,41,0.35)';
+
+    }
 }
 function MyFunc_1(){
     var count=parseInt(document.getElementById('number1').value,10);
@@ -46,16 +53,24 @@ function increment_value_2(){
     document.getElementById('number2').value=count;
     document.getElementById('number_quant_2').innerHTML=count;
 
+
 }
 function decrement_value_2(){
     var count=parseInt(document.getElementById('number2').value,10);
-    
+   
     if(count>0) count=count-1;
     document.getElementById('number2').value=count;
     document.getElementById('number_quant_2').innerHTML=count;
+    if(count==0){
+        document.getElementById('number_quant_2').style.color='rgba(33,37,41,0.35)';
+        document.getElementById('temp_quant_2').style.color='rgba(33,37,41,0.35)';
+
+    }
 }
 
 function increment_value_3(){
+    document.getElementById('number_quant_3').style.color='black';
+    document.getElementById('temp_quant_3').style.color='black';
     
     var count=parseInt(document.getElementById('number3').value,10);
     
@@ -67,12 +82,21 @@ function increment_value_3(){
 function decrement_value_3(){
     var count=parseInt(document.getElementById('number3').value,10);
     
+   
+
     if(count>0) count=count-1;
     document.getElementById('number3').value=count;
     document.getElementById('number_quant_3').innerHTML=count;
+    if(count==0){
+        document.getElementById('number_quant_3').style.color='rgba(33,37,41,0.35)';
+        document.getElementById('temp_quant_3').style.color='rgba(33,37,41,0.35)';
+
+    }
 }
 
 function increment_value_4(){
+    document.getElementById('number_quant_4').style.color='black';
+    document.getElementById('temp_quant_4').style.color='black';
     
     var count=parseInt(document.getElementById('number4').value,10);
     
@@ -84,12 +108,21 @@ function increment_value_4(){
 function decrement_value_4(){
     var count=parseInt(document.getElementById('number4').value,10);
     
+   
+
     if(count>0) count=count-1;
     document.getElementById('number4').value=count;
     document.getElementById('number_quant_4').innerHTML=count;
+    if(count==0){
+        document.getElementById('number_quant_4').style.color='rgba(33,37,41,0.35)';
+        document.getElementById('temp_quant_4').style.color='rgba(33,37,41,0.35)';
+
+    }
 }
 
 function increment_value_5(){
+    document.getElementById('number_quant_5').style.color='black';
+    document.getElementById('temp_quant_5').style.color='black';
     
     var count=parseInt(document.getElementById('number5').value,10);
     
@@ -101,13 +134,21 @@ function increment_value_5(){
 function decrement_value_5(){
     var count=parseInt(document.getElementById('number5').value,10);
     
+  
+
     if(count>0) count=count-1;
     document.getElementById('number5').value=count;
     document.getElementById('number_quant_5').innerHTML=count;
+    if(count==0){
+        document.getElementById('number_quant_5').style.color='rgba(33,37,41,0.35)';
+        document.getElementById('temp_quant_5').style.color='rgba(33,37,41,0.35)';
+
+    }
 }
 
 function increment_value_6(){
-    
+    document.getElementById('number_quant_6').style.color='black';
+    document.getElementById('temp_quant_6').style.color='black';
     var count=parseInt(document.getElementById('number6').value,10);
     
     if(count<9) count=count+1;
@@ -118,7 +159,14 @@ function increment_value_6(){
 function decrement_value_6(){
     var count=parseInt(document.getElementById('number6').value,10);
     
+    
+
     if(count>0) count=count-1;
     document.getElementById('number6').value=count;
     document.getElementById('number_quant_6').innerHTML=count;
+    if(count==0){
+        document.getElementById('number_quant_6').style.color='rgba(33,37,41,0.35)';
+        document.getElementById('temp_quant_6').style.color='rgba(33,37,41,0.35)';
+
+    }
 }
