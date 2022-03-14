40f1371244849f0682837899dad2c2c211d1e678
SARTHAK811 <82260982+SARTHAK811@users.noreply.github.com>
2022-03-14 19:45:56 +0530

    Dynamic addition of prices for checking out

--- Backend/templates/mess_prof_assets/js/mess.js
+++ Backend/templates/mess_prof_assets/js/mess.js
@@ -1,3 +1,10 @@
+let total1=0;
+let total2=0;
+let total3=0;
+let total4=0;
+let total5=0;
+let total6=0;
+
 function increment_value_1(){
     document.getElementById('number_quant_1').style.color='black';
     document.getElementById('temp_quant_1').style.color='black';
@@ -7,6 +14,8 @@ function increment_value_1(){
     if(count<9) count=count+1;
     document.getElementById('number1').value=count;
     document.getElementById('number_quant_1').innerHTML=count;
+    total1= count*30;
+    document.getElementById('Total_Extras').innerHTML=total1+total2+total3+total4+total5+total6;
 
 }
 function decrement_value_1(){
@@ -20,6 +29,8 @@ function decrement_value_1(){
         document.getElementById('temp_quant_1').style.color='rgba(33,37,41,0.35)';
 
     }
+    total1=count*30;
+    document.getElementById('Total_Extras').innerHTML=total1+total2+total3+total4+total5+total6;
 }
 function MyFunc_1(){
     var count=parseInt(document.getElementById('number1').value,10);
@@ -52,6 +63,8 @@ function increment_value_2(){
     if(count<9) count=count+1;
     document.getElementById('number2').value=count;
     document.getElementById('number_quant_2').innerHTML=count;
+    total2=count*30;
+    document.getElementById('Total_Extras').innerHTML=total1+total2+total3+total4+total5+total6;
 
 
 }
@@ -66,6 +79,8 @@ function decrement_value_2(){
         document.getElementById('temp_quant_2').style.color='rgba(33,37,41,0.35)';
 
     }
+    total2=count*30;
+    document.getElementById('Total_Extras').innerHTML=total1+total2+total3+total4+total5+total6;
 }
 
 function increment_value_3(){
@@ -77,6 +92,9 @@ function increment_value_3(){
     if(count<9) count=count+1;
     document.getElementById('number3').value=count;
     document.getElementById('number_quant_3').innerHTML=count;
+
+    total3= count*30;
+    document.getElementById('Total_Extras').innerHTML=total1+total2+total3+total4+total5+total6;
 
 }
 function decrement_value_3(){
@@ -92,6 +110,8 @@ function decrement_value_3(){
         document.getElementById('temp_quant_3').style.color='rgba(33,37,41,0.35)';
 
     }
+    total3=count*30;
+    document.getElementById('Total_Extras').innerHTML=total1+total2+total3+total4+total5+total6;
 }
 
 function increment_value_4(){
@@ -103,6 +123,9 @@ function increment_value_4(){
     if(count<9) count=count+1;
     document.getElementById('number4').value=count;
     document.getElementById('number_quant_4').innerHTML=count;
+
+    total4= count*30;
+    document.getElementById('Total_Extras').innerHTML=total1+total2+total3+total4+total5+total6;
 
 }
 function decrement_value_4(){
@@ -118,6 +141,8 @@ function decrement_value_4(){
         document.getElementById('temp_quant_4').style.color='rgba(33,37,41,0.35)';
 
     }
+    total4=count*30;
+    document.getElementById('Total_Extras').innerHTML=total1+total2+total3+total4+total5+total6;
 }
 
 function increment_value_5(){
@@ -129,6 +154,9 @@ function increment_value_5(){
     if(count<9) count=count+1;
     document.getElementById('number5').value=count;
     document.getElementById('number_quant_5').innerHTML=count;
+
+    total5= count*30;
+    document.getElementById('Total_Extras').innerHTML=total1+total2+total3+total4+total5+total6;
 
 }
 function decrement_value_5(){
@@ -144,6 +172,8 @@ function decrement_value_5(){
         document.getElementById('temp_quant_5').style.color='rgba(33,37,41,0.35)';
 
     }
+    total5=count*30;
+    document.getElementById('Total_Extras').innerHTML=total1+total2+total3+total4+total5+total6;
 }
 
 function increment_value_6(){
@@ -154,6 +184,9 @@ function increment_value_6(){
     if(count<9) count=count+1;
     document.getElementById('number6').value=count;
     document.getElementById('number_quant_6').innerHTML=count;
+
+    total6= count*30;
+    document.getElementById('Total_Extras').innerHTML=total1+total2+total3+total4+total5+total6;
 
 }
 function decrement_value_6(){
@@ -169,4 +202,7 @@ function decrement_value_6(){
         document.getElementById('temp_quant_6').style.color='rgba(33,37,41,0.35)';
 
     }
+
+    total6=count*30;
+    document.getElementById('Total_Extras').innerHTML=total1+total2+total3+total4+total5+total6;
 }
