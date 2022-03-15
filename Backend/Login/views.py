d5671f577dff79a1bf7b5dc7c2b9f29be3624cc8
Aryan Vora <aryanv20@iitk.ac.in>
2022-03-15 13:45:38 +0530

    Login redirects to correct profile page

--- Backend/Login/views.py
+++ Backend/Login/views.py
@@ -6,19 +6,23 @@ from .models import Profile
 
 def login_view(request,*args,**kwargs):
     profiles = Profile.objects.all()
-    for pro in profiles:
-        print (pro.login)
-        if pro.login == True:
-            return redirect("../profile/")
+    # for pro in profiles:
+    #     print (pro.login)
+    #     if pro.login == True:
+    #         return redirect("../profile/")
     if request.method == "POST" :
         user = request.POST.get('User ID')
         passw = request.POST.get('password')
         #pro = profile.ob
         #profiles = Profile.objects.all()
+        userid=0
         for pro in profiles:
+            userid=userid+1
             if pro.username == user and pro.password ==passw:
                 pro.login = True
-                return redirect("../profile/")
+                
+                return redirect("../profile/"+str(userid))
+            
         #print(username+ " " + password)
     return render(request,"login.html",{})
 
