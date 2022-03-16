84aaba9ab8fe316ca328b44224b225a1f29f091b
Aryan Vora <aryanv20@iitk.ac.in>
2022-03-16 23:06:35 +0530

    Login Ho raha

--- Backend/Login/views.py
+++ Backend/Login/views.py
@@ -2,31 +2,35 @@ from django.shortcuts import render, redirect
 #from .serializers import ProfileSerializer
 from .models import Profile
 #from rest_framework import viewsets
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
-def login_view(request,*args,**kwargs):
-    profiles = Profile.objects.all()
-    # for pro in profiles:
-    #     print (pro.login)
-    #     if pro.login == True:
-    #         return redirect("../profile/")
-    if request.method == "POST" :
-        user = request.POST.get('User ID')
-        passw = request.POST.get('password')
-        #pro = profile.ob
-        #profiles = Profile.objects.all()
-        userid=0
-        for pro in profiles:
-            userid=userid+1
-            if pro.username == user and pro.password ==passw:
-                pro.login = True
+
+# def login_view(request,*args,**kwargs):
+    # profiles = Profile.objects.all()
+    # # for pro in profiles:
+    # #     print (pro.login)
+    # #     if pro.login == True:
+    # #         return redirect("../profile/")
+    # if request.method == "POST" :
+    #     user = request.POST.get('User ID')
+    #     passw = request.POST.get('password')
+    #     #pro = profile.ob
+    #     #profiles = Profile.objects.all()
+    #     userid=0
+    #     for pro in profiles:
+    #         userid=userid+1
+    #         if pro.username == user and pro.password ==passw:
+    #             pro.login = True
                 
-                return redirect("../profile/"+str(userid))
+    #             return redirect("../profile/"+str(userid))
             
-        #print(username+ " " + password)
-    return render(request,"login.html",{})
+    #     #print(username+ " " + password)
+    # return render(request,"login.html",{})
 
-
+    # form = UserCreationForm()
+    # return render(request,"login.html",{'form':form})
+    
 def profile_view(request,id):
     user=Profile.objects.get(id=id)
     context={"username":user.username,"name":user.full_name}
--- Backend/src/settings.py
+++ Backend/src/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'Login',
     'Mess',
     'rest_framework',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL='profile'
