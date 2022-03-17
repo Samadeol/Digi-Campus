544bb14e9a6882f77c582062bd9ad847111041ce
Aryan Vora <aryanv20@iitk.ac.in>
2022-03-17 14:54:54 +0530

    Mess stuff

--- Backend/Mess/models.py
+++ Backend/Mess/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-class messMenu(models.Model) :
-    itemName = models.CharField(max_length=25)
-    itemPrice = models.IntegerField()
+class messMenu(models.Model):
+    itemName = models.CharField(max_length=25,null=True)
+    itemPrice = models.IntegerField(null=True)
+
+class OrderHistory(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    itemName = models.CharField(max_length=25,null=True)
+    itemPrice = models.IntegerField(null=True)
