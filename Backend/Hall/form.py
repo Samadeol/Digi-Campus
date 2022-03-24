a2b2b0affef4b41562d27c960e3696bdf2480b8b
Aryan Vora <aryanv20@iitk.ac.in>
2022-03-24 14:07:57 +0530

    Entry ka sahi kiya

--- Backend/Hall/form.py
+++ Backend/Hall/form.py
@@ -3,9 +3,13 @@ from sympy import Q
 from .models import HallStudents, hallPresence
 from Login.models import Profile
 from django.contrib.auth.models import User
+Choices=[
+    ('Yes','Yes'),
+    ('No','No')
+]
 class EntryForm(forms.Form):
     user_visiting=forms.CharField(label='Roll Number Visiting',widget=forms.TextInput(attrs={"placeholder":"Roll Number Visiting"}))
-    laptop=forms.CharField(label='Laptop (Yes/No)',widget=forms.TextInput(attrs={"placeholder":"Yes/No"}))
+    laptop=forms.CharField(label='Laptop (Yes/No)',widget=forms.Select(choices=Choices))
 
 class ExitForm(forms.Form):
     exit=forms.BooleanField(label='Do you want to exit?',widget=forms.BooleanField())
