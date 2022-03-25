12c78792b5ed729b752b5b7a1bbf2fc4cc75bfbe
SARTHAK811 <82260982+SARTHAK811@users.noreply.github.com>
2022-03-25 16:36:01 +0530

    logout

--- src/settings.py
+++ src/settings.py
@@ -139,6 +139,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_URL='/login/'
+LOGIN_URL='/'
 LOGIN_REDIRECT_URL='/profile/'
 LOGOUT_REDIRECT_URL='/logout/'
