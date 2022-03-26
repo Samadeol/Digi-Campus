0175e154a0e49a56898f9bb5bbfaf282a454fdce
Shashwat Gupta <guptashashwatme@gmail.com>
2022-03-26 10:59:07 +0530

    Update settings.py

--- Backend/src/settings.py
+++ Backend/src/settings.py
@@ -138,10 +138,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL='/login/'
 LOGIN_REDIRECT_URL='/profile/'
 
-EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+# EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'your_email'
-EMAIL_HOST_PASSWORD = 'your password'
+EMAIL_HOST_USER = 'digicampus352@gmail.com'
+EMAIL_HOST_PASSWORD = 'digi@campus'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_PORT = 465
+EMAIL_PORT = 587
