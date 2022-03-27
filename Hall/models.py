ddd48a17f1b8007c841777da766d9af33a40f3ac
Samarth Arora <samartharora03@gmail.com>
2022-03-27 11:40:20 +0530

    askl

--- Hall/models.py
+++ Hall/models.py
@@ -7,6 +7,7 @@ class hallPresence(models.Model):
     hall_numnber = models.IntegerField()
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     user_visiting=models.IntegerField(null=True)
+    room_visiting = models.CharField(max_length=4,null=True)
     in_hall=models.BooleanField(default=False)
     laptop=models.BooleanField(default=False)
     timeEntered=models.DateTimeField(null=True)
