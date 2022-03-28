1b010ccfe6cf6ab4d5902137a30dd461cc6746c8
SARTHAK811 <82260982+SARTHAK811@users.noreply.github.com>
2022-03-28 17:04:17 +0530

    sarthak stealing my commitsd

--- Hall/urls.py
+++ Hall/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.apiOverview, name="api"),
+    path('',views.apiOverview, name="api_security"),
     path('student-list/',views.studentlist, name="student-list"),
     path('student-detail/<str:pk>',views.studentdetail, name="student-detail"),
     path('student-create/',views.studentcreate, name="student-create"),
